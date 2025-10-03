1b9cb11c2d683bfc90887fb8320bf29b972512bc
leandro-etec-2025 <leandrocesardacruz.cps.2025@gmail.com>
2025-10-03 10:37:17 -0300

    Finalizado classe informações

--- src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
+++ src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
@@ -19,4 +19,58 @@ public class ClasseInformacoes {
 		this.titulosAt = titulo;
 	}
 	
+	// Getters
+	public String getTitulo(){
+		return titulosAt;
+	}
+	
+	public String getSubTitulo(){
+		return subtitulosAt;
+	}
+	
+	public String getAutores(){
+		return autoresAt;
+	}
+	
+	public String getGeneros(){
+		return generosAt;
+	}
+	
+	public int getNumeroPaginas(){
+		return numerosPaginasAt;
+	}
+	
+	// Setters
+	public void setAutores(String name){
+		this.autoresAt = name;
+	}
+	
+	public void setTitulos(String title){
+		this.titulosAt = title;
+	}
+	
+	public void setSubTitulos(String subtitle){
+		this.subtitulosAt = subtitle;
+	}
+	
+	public void setGeneros(String gender){
+		this.generosAt = gender;
+	}
+	
+	public void setNumerosPaginas(int page){
+		this.numerosPaginasAt = page;
+	}
+	
+	// Método próprio
+	public void mostrarDados(){
+		System.out.println("Informações do Produto:");  // sysout + ctrl + space
+		System.out.println(titulosAt);
+		System.out.println(subtitulosAt);
+		System.out.println(autoresAt);
+		System.out.println(generosAt);
+		System.out.println(numerosPaginasAt);
+	}
+	
+	
+	
 }
